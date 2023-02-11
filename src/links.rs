93543a6b20f0dce95c41050c58b8a8d01a334cd7
jepemo <jeremiespm@gmail.com>
2023-02-11 23:08:19 +0100

    added serialization library to create link structure

--- src/links.rs
+++ src/links.rs
@@ -1,5 +1,29 @@
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 use std::fs::File;
+// use std::fs::OpenOptions;
 use std::path::Path;
+
+#[derive(Serialize, Deserialize)]
+struct Link {
+    url: String,
+    description: String,
+    tags: Vec<String>,
+}
+
+impl Link {
+    fn new(url: String, description: String, tags: String) -> Link {
+        let cleaned_tags = tags
+            .split(",")
+            .map(|tag| String::from(tag.trim()))
+            .collect();
+        Link {
+            url,
+            description,
+            tags: cleaned_tags,
+        }
+    }
+}
 
 pub fn handle_link(data: String, add: Option<String>) {
     let links_file = Path::new(&data).join("links.jsonl");
@@ -10,7 +34,31 @@ pub fn handle_link(data: String, add: Option<String>) {
     }
 
     if add.is_some() {
-        println!("Add link {:?}", add.unwrap())
+        let url = add.unwrap();
+        let link = Link::new(
+            url,
+            String::from("this is a description"),
+            String::from("1, 2, 3, 4"),
+        );
+
+        // read json file and group into a map by url
+
+        // check if the url exist
+        // -> show a meesage and exit if exists
+
+        // add the new link in the map
+
+        // append the link in the file
+        // or?
+        // write again the map in the file
+        let json_link = json!(link);
+        println!("Add link {:?}", json_link);
+
+        // let mut file = OpenOptions::new().append(true).open(links_file).unwrap();
+
+        // if let Err(e) = writeln!(file, "{}", json_link) {
+        //     eprintln!("Couldn't write to file: {}", e);
+        // }
     } else {
         println!("Show list")
     }
